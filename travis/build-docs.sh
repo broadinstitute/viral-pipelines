2ec89f21a2be2a29a06775496f491a640f0cf751
Danny Park <dpark@broadinstitute.org>
2019-11-05 12:25:25 -0500

    re-add docs building, re-add wdl versioning mechanism, move more workflows into compile space

--- travis/build-docs.sh
+++ travis/build-docs.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-set -e -o pipefail
+set -e
 
 if [[ ( -n $TRAVIS_PULL_REQUEST && $TRAVIS_PULL_REQUEST != "false" ) || $TRAVIS_BRANCH = "master" || -n "$TRAVIS_TAG" ]]; then
+
+	while IFS='=' read module version; do
+		git clone --depth=1 https://github.com/$module.git
+		pushd $module
+		git checkout -qf $version
+		popd
+		cp -a $module/* .
+	done < $MODULE_VERSIONS
+
     pushd docs
     make html && echo "Docs built successfully!" || echo "Docs did NOT build successfully."
     popd
---
+++ travis/version-wdl-runtimes.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# requires $MODULE_VERSIONS to be set to point to a text file with equal-sign-separated values
+set -e -o pipefail
+
+while IFS='=' read module version; do
+	STRIP_VERSION=`echo $version | sed 's/^v//'`
+	OLD_TAG=$module
+	NEW_TAG="$module:$STRIP_VERSION"
+	echo Replacing $OLD_TAG with $NEW_TAG in all task WDL files
+	sed -i -- "s|$OLD_TAG|$NEW_TAG|g" pipes/WDL/tasks/*.wdl
+done < $MODULE_VERSIONS
