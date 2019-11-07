4b27bc559c36edc533ce959311e912600d4c0dae
Danny Park <dpark@broadinstitute.org>
2019-11-07 10:36:16 -0500

    docs simplify

--- travis/build-docs.sh
+++ travis/build-docs.sh
@@ -2,18 +2,6 @@
 
 set -e
 
-if [[ ( -n $TRAVIS_PULL_REQUEST && $TRAVIS_PULL_REQUEST != "false" ) || $TRAVIS_BRANCH = "master" || -n "$TRAVIS_TAG" ]]; then
-
-	while IFS='=' read module version; do
-		git clone --depth=1 https://github.com/$module.git
-		DIR=`echo $module | cut -f 2 -d /`
-		pushd $DIR
-		git checkout -qf $version
-		popd
-		cp -a $DIR/* .
-	done < $MODULE_VERSIONS
-
-    pushd docs
-    make html && echo "Docs built successfully!" || echo "Docs did NOT build successfully."
-    popd
-fi
+pushd docs
+make html && echo "Docs built successfully!" || echo "Docs did NOT build successfully."
+popd
