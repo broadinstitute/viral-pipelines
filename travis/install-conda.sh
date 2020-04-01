f6f705ee7d0f43b09daf4f329d69652af9c93f72
Danny Park <dpark@broadinstitute.org>
2020-04-01 09:48:31 -0400

    verbosity for travis

--- travis/install-conda.sh
+++ travis/install-conda.sh
@@ -7,12 +7,14 @@ if [ -d "$MINICONDA_DIR" ] && [ -e "$MINICONDA_DIR/bin/conda" ]; then
     export PATH="$MINICONDA_DIR/bin:$PATH"
     hash -r
 else # if it does not exist, we need to install miniconda
+    echo "Downloading conda to $MINICONDA_DIR"
     rm -rf "$MINICONDA_DIR" # remove the directory in case we have an empty cached directory
     curl -S https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh > miniconda.sh;
     bash miniconda.sh -b -p "$MINICONDA_DIR"
     chown -R "$USER" "$MINICONDA_DIR"
     export PATH="$MINICONDA_DIR/bin:$PATH"
     hash -r
+    echo "Installing conda"
     conda install -y conda==4.6.14 # specify "conda update -c conda-canary conda" for pre-release conda
     conda config --set always_yes yes --set changeps1 no --set remote_max_retries 6 #--set channel_priority strict
     conda config --add channels defaults
@@ -23,10 +25,11 @@ else # if it does not exist, we need to install miniconda
     # Use recommendations from https://github.com/bioconda/bioconda-recipes/issues/13774
     #conda update --quiet -y conda
     # conda config --set channel_priority strict
-
-    conda create -y -m --quiet -p $CONDA_DEFAULT_ENV python="$TRAVIS_PYTHON_VERSION"
+    echo "Creating conda default env in $CONDA_DEFAULT_ENV"
+    conda create -y -m -p $CONDA_DEFAULT_ENV python="$TRAVIS_PYTHON_VERSION"
 fi
 
+echo "Activating conda environment: $CONDA_DEFAULT_ENV"
 source activate $CONDA_DEFAULT_ENV
 # update certs
 conda info -a # for debugging
