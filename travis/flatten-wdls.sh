b1de97f02e877582e5ae63bb9f62c00c0466121f
Danny Park <dpark@broadinstitute.org>
2020-04-01 10:32:22 -0400

    more fixes

--- travis/flatten-wdls.sh
+++ travis/flatten-wdls.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e -o pipefail
 
-FLAT_DIR=../pipes/WDL/flattened
+FLAT_DIR=pipes/WDL/flattened
 mkdir -p $FLAT_DIR
 
-for workflow in ../pipes/WDL/workflows/*.wdl; do
+for workflow in pipes/WDL/workflows/*.wdl; do
 	wf_base=`basename $workflow`
 	out_fn="$FLAT_DIR/$wf_base"
 	echo "flattening $workflow to $out_fn"
