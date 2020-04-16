112fadc1426a7044cec1ae706b866f9ac9adc8e6
Danny Park <dpark@broadinstitute.org>
2020-04-16 14:58:24 -0400

    hmm... need to make the dir before installing conda

--- travis/install-conda.sh
+++ travis/install-conda.sh
@@ -10,6 +10,7 @@ else # if it does not exist, we need to install miniconda
     echo "Downloading conda to $MINICONDA_DIR"
     rm -rf "$MINICONDA_DIR" "$CONDA_DEFAULT_ENV" # remove the directory in case we have an empty cached directory
     curl -S https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh > miniconda.sh;
+    mkdir -p "$MINICONDA_DIR"
     bash miniconda.sh -b -p "$MINICONDA_DIR"
     chown -R "$USER" "$MINICONDA_DIR"
     export PATH="$MINICONDA_DIR/bin:$PATH"
