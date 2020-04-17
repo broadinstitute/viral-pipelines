9c4e0cb044d60c7f7308acdb5f61319c72fe0b04
Danny Park <dpark@broadinstitute.org>
2020-04-16 20:38:02 -0400

    add output validation for miniwdl integration tests

--- travis/tests-miniwdl.sh
+++ travis/tests-miniwdl.sh
@@ -10,22 +10,32 @@ miniwdl run_self_test
 for workflow in ../pipes/WDL/workflows/*.wdl; do
 	workflow_name=$(basename $workflow .wdl)
 	input_json="test/input/WDL/test_inputs-$workflow_name-local.json"
+	expected_output_json="test/input/WDL/test_outputs-$workflow_name-local.json"
 	if [ -f $input_json ]; then
 		date
-		echo "validating $workflow"
-		miniwdl check $workflow
 		echo "Executing $workflow_name using miniWDL on local instance"
-		miniwdl run -i $input_json -d $workflow_name/. --verbose --error-json $workflow
+		miniwdl run -i $input_json -d $workflow_name/. --error-json $workflow
 		if [ -f $workflow_name/outputs.json ]; then
-			echo "$workflow_name SUCCESS -- outputs:"
-			cat $workflow_name/outputs.json
+			if [ -f $expected_output_json ]; then
+				echo "$workflow_name SUCCESS -- validating outputs"
+				touch expected actual
+				for k in `cat $expected_output_json | jq -r 'keys[]'`; do
+					echo -n "$k=" >> expected
+					echo -n "$k=" >> actual
+					cat $expected_output_json       | jq -r '.["'$k'"]' >> expected
+					cat $workflow_name/outputs.json | jq -r '.outputs["'$k'"]' >> actual
+				done
+				diff expected actual
+			else
+				echo "$workflow_name SUCCESS -- outputs:"
+				cat $workflow_name/outputs.json
+			fi
 		else
 			echo "$workflow_name FAILED"
 			exit 1
 		fi
-    fi
+	fi
 done
 
 cd -
 date
-echo "note: there is no testing of output correctness yet..."
