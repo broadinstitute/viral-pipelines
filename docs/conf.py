792758983915f19dac69ae51d6645d2b98d06c04
Danny Park <dpark@broadinstitute.org>
2020-04-22 21:13:31 -0400

    fixes

--- docs/conf.py
+++ docs/conf.py
@@ -43,12 +43,11 @@ __version__ = _git_version()
 def _make_wdl_markdowns():
     wf_dir = '../pipes/WDL/workflows'
     workflows = os.listdir(wf_dir)
-    for wf in workflows:
+    for wf in sorted(workflows):
         fname_base = os.path.basename(wf)
         if fname_base.endswith('.wdl'):
             fname_base = fname_base[:-4]
-            subprocess.check_call(['wdl-aid', '-o', fname_base+'.md', os.path.join(wf_dir, wf)])
-            # add a line to workflows.rst about fname_base+'.md'
+            subprocess.check_call(['wdl-aid', '-o', fname_base+'.md', os.path.join(wf_dir, fname_base)+'.wdl'])
             with open('workflows.rst', 'a') as outf:
                 outf.write(" - `{} <{}.md>`_\n".format(fname_base, fname_base))
 _make_wdl_markdowns()
