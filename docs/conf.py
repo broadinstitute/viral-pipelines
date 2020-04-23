27f327952ea191868fe44b22f2d37c4877ed869e
Danny Park <dpark@broadinstitute.org>
2020-04-23 07:01:38 -0400

    add .md as a filename to collect

--- docs/conf.py
+++ docs/conf.py
@@ -66,7 +66,7 @@ extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.imgmath', 
 templates_path = ['_templates']
 
 # The suffix of source filenames.
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
 
 # The encoding of source files.
 #source_encoding = 'utf-8-sig'
