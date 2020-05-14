c7565e22a98338234bcb4d3d311e26a637eb4e5b
Danny Park <dpark@broadinstitute.org>
2020-05-14 15:46:51 -0400

    update dxWDL 1.46.4 to 1.47

--- travis/install-wdl.sh
+++ travis/install-wdl.sh
@@ -19,7 +19,7 @@ cached_fetch_jar_from_github () {
 
 cached_fetch_jar_from_github broadinstitute cromwell womtool 49
 cached_fetch_jar_from_github broadinstitute cromwell cromwell 49
-cached_fetch_jar_from_github dnanexus dxWDL dxWDL v1.46.4
+cached_fetch_jar_from_github dnanexus dxWDL dxWDL v1.47
 
 TGZ=dx-toolkit-v0.293.0-ubuntu-16.04-amd64.tar.gz
 if [ ! -f $CACHE_DIR/$TGZ ]; then
