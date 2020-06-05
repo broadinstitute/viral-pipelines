08877b0d7d2d819b9842b8b62f2665c555a3082a
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2020-06-05 17:43:41 -0400

    debug statement

--- travis/build-dx.sh
+++ travis/build-dx.sh
@@ -60,6 +60,7 @@ echo -e "consolidate_run_tarballs\t$dx_id" >> $COMPILE_SUCCESS
 # demux workflow ID as a default input 
 demux_workflows_to_build="demux_plus demux_only"
 for wf_name in $(echo "${demux_workflows_to_build}"); do
+  echo "Building applet ${wf_name...}"
   demux_workflow_id=$(grep "${wf_name}" $COMPILE_SUCCESS | cut -f 2)  
   sed "s/DEFAULT_DEMUX_WORKFLOW_ID/$demux_workflow_id/" demux_launcher.yml \
     | sed "s/DEFAULT_DEMUX_WORKFLOW_NAME/${wf_name}_launcher/" \
