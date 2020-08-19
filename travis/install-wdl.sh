38cf1b219143b1f9df7470c9502287ebd98846ff
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2020-08-19 03:12:20 -0400

    roll back to dxWDL v1.48

--- travis/install-wdl.sh
+++ travis/install-wdl.sh
@@ -19,7 +19,7 @@ cached_fetch_jar_from_github () {
 
 cached_fetch_jar_from_github broadinstitute cromwell womtool 49
 cached_fetch_jar_from_github broadinstitute cromwell cromwell 49
-cached_fetch_jar_from_github dnanexus dxWDL dxWDL 2.0.0-rc2
+cached_fetch_jar_from_github dnanexus dxWDL dxWDL v1.48
 
 TGZ=dx-toolkit-v0.298.1-ubuntu-20.04-amd64.tar.gz
 if [ ! -f $CACHE_DIR/$TGZ ]; then
