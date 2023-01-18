e399fcdfe029d4c4e1976a1de2752323737fa20c
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2023-01-18 13:09:19 -0500

    try always explicitly specifying the sphinx_rtd_theme

--- docs/conf.py
+++ docs/conf.py
@@ -77,7 +77,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'viral-ngs'
-copyright = '2014, Broad Institute Viral Genomics'
+copyright = '2023, Broad Institute Viral Genomics'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -128,13 +128,17 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-on_rtd = os.environ.get('READTHEDOCS') == 'True'
-if on_rtd:
-    html_theme = 'default'
-else:
-    import sphinx_rtd_theme
-    html_theme = "sphinx_rtd_theme"
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# on_rtd = os.environ.get('READTHEDOCS') == 'True'
+# if on_rtd:
+#     html_theme = 'default'
+# else:
+#     import sphinx_rtd_theme
+#     html_theme = "sphinx_rtd_theme"
+#     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+import sphinx_rtd_theme
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
