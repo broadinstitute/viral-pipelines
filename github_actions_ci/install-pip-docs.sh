8ece33e78de79425e12f9074c25ca4a30e725274
Danny Park <dpark@broadinstitute.org>
2024-07-25 10:16:43 -0400

    make pip install quiet again

--- github_actions_ci/install-pip-docs.sh
+++ github_actions_ci/install-pip-docs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "pip installing test-related packages (Sphinx, etc.)"
-pip install -r docs/requirements.txt
+pip install --quiet -r docs/requirements.txt
 
 # list what was installed
 pip freeze
